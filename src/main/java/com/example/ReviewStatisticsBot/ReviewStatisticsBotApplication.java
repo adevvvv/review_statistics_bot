9e0fdc9539f00526bc499866bd9d4a967bd5b552
adevvvv <adevvv@bk.ru>
2024-03-04 20:13:40 +0300

    feature: Создать пустой проект для старта

---
+++ src/main/java/com/example/ReviewStatisticsBot/ReviewStatisticsBotApplication.java
@@ -0,0 +1,13 @@
+package com.example.ReviewStatisticsBot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReviewStatisticsBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReviewStatisticsBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ReviewStatisticsBot/ReviewStatisticsBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ReviewStatisticsBot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReviewStatisticsBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
