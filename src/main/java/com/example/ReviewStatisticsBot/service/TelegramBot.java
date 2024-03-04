d23f74f72b0610682884993c96ba270763238dd4
adevvvv <adevvv@bk.ru>
2024-03-04 21:21:06 +0300

    feature: Добавить меню и инструкцию (/help)

--- src/main/java/com/example/ReviewStatisticsBot/service/TelegramBot.java
+++ src/main/java/com/example/ReviewStatisticsBot/service/TelegramBot.java
@@ -4,10 +4,15 @@ import com.example.ReviewStatisticsBot.config.BotConfig;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.commands.SetMyCommands;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.commands.BotCommand;
+import org.telegram.telegrambots.meta.api.objects.commands.scope.BotCommandScopeDefault;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
+import java.util.ArrayList;
+import java.util.List;
 
 
 @Slf4j
@@ -15,8 +20,21 @@ import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 public class TelegramBot extends TelegramLongPollingBot {
 
     final BotConfig config;
+    static final String HELP_TEXT = "Этот бот создан для того, чтобы мы стали лучше. Оставь пожалуйста свой отзыв о продукте и получи в конце скидку 50% на следующую покупку.\n\n" +
+            "Вы можете выполнять команды из главного меню слева или набрав команду:\n\n" +
+            "Введите /start, чтобы увидеть приветственное сообщение\n\n" +
+            "Введите /help, чтобы снова увидеть это сообщение";
+    static final String ERROR_TEXT = "Error occurred: ";
     public TelegramBot(BotConfig config) {
         this.config = config;
+        List<BotCommand> listOfCommands = new ArrayList<>();
+        listOfCommands.add(new BotCommand("/start", "приветственное сообщение"));
+        listOfCommands.add(new BotCommand("/help", "инструкция"));
+        try {
+            this.execute(new SetMyCommands(listOfCommands, new BotCommandScopeDefault(), null));
+        } catch (TelegramApiException e) {
+            log.error("Error setting bot's command list: " + e.getMessage());
+        }
     }
     @Override
     public String getBotUsername() {
@@ -34,6 +52,11 @@ public class TelegramBot extends TelegramLongPollingBot {
             switch (messageText) {
                 case "/start":
                     startCommandReceived(chatId, update.getMessage().getChat().getFirstName());
+                    break;
+
+                case "/help":
+
+                    prepareAndSendMessage(chatId, HELP_TEXT);
                     break;
                 default:
                     sendMessage(chatId, "Извините, команда не распознана");
@@ -56,5 +79,19 @@ public class TelegramBot extends TelegramLongPollingBot {
 
             log.error("Error occurred: " + e.getMessage());
         }
+    }
+    private void executeMessage(SendMessage message){
+        try {
+            execute(message);
+        } catch (TelegramApiException e) {
+            log.error(ERROR_TEXT + e.getMessage());
+        }
+    }
+
+    private void prepareAndSendMessage(long chatId, String textToSend){
+        SendMessage message = new SendMessage();
+        message.setChatId(String.valueOf(chatId));
+        message.setText(textToSend);
+        executeMessage(message);
     }
 }
